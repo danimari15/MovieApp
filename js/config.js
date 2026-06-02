4dbf4ea537a7127b85e04269bd382bb6fc067928
danimari15 <daniele.marinelli@itstechtalentfactory.it>
2026-06-02 22:29:16 +0200

    fix bug

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// info per connessione API e gestione immagini
+const API_KEY = "9f77a537eb067b29b324340d3ba5c91c";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
